1fbe55530edd1a6f6cd110ce356b1c74c72297fc
Aziz Azizli <aziz.azizzadeh@gmail.com>
2019-09-04 17:30:06 +0400

    fileutil-added objectbyobject read/write

--- Competitor/src/util/FileUtil.java
+++ Competitor/src/util/FileUtil.java
@@ -29,4 +29,24 @@ public class FileUtil {
             return obj;
         }
     }
+
+    public static void writeObjectByObject(Object[] objects, String fileName, boolean append) throws Exception{
+        try(FileOutputStream fos = new FileOutputStream(fileName, append); ObjectOutputStream oos = new ObjectOutputStream(fos)){
+            for(Object obj : objects){
+                oos.writeObject(obj);
+            }
+        }
+    }
+
+    public static Object[] readObjectByObject(int objCount, String fileName) throws Exception{
+        Object[] objects = new Object[objCount];
+        try(FileInputStream fis = new FileInputStream(fileName); ObjectInputStream ois = new ObjectInputStream(fis)){
+            for (int i=0; i<objCount; i++){
+                Object obj = ois.readObject();
+                objects[i] = obj;
+            }
+        }
+
+        return objects;
+    }
 }
